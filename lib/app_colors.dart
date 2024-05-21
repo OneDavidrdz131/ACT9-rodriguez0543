c2aec117857a13eb7c3a775679b9f04b39c427c1
DAVID_RODRIGUEZ_JURADO <a.21308051280543@cbtis128.edu.mx>
2024-05-21 01:48:08 +0000

    version3

---
+++ lib/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+final Color background = Color(0xFFfafafc);
+final Color sliverbackground = Color(0xFFfafafc);
+final Color menu1color = Color(0xff0640e1);
+final Color menu2color = Color(0xff1872d4);
+final Color menu3color = Color(0xff2585ea);
+
+final Color tabBarViewColor = Color(0xfffdfdfd);
+final Color starcolor = Color(0xffdcae14);
+final Color subtitletext = Color(0xff929197);
+final Color lovecolor = Color(0xff006efe);
